553ef57a24ccfa84e21bd65e4ce0036e0410b2aa
xinxgchn <xinxgchn@163.com>
2016-04-08 09:19:07 +0800

    test

--- src/com/zxxk/test/UserTest.java
+++ src/com/zxxk/test/UserTest.java
@@ -3,7 +3,8 @@ package com.zxxk.test;
 public class UserTest {
 
 	public static void main(String[] args) {
-		System.out.print("Hello");
+		System.out.println("Hello");
+		System.out.println("Hello scott");
 	}
 
 }
