3707012c8dcf69bbbf4a3533ffbcb5231d223835
xinxgchn <xinxgchn@163.com>
2016-04-08 09:41:52 +0800

    测试下刚才那个问题，xxg，2016年4月8日

--- src/com/zxxk/test/UserTest.java
+++ src/com/zxxk/test/UserTest.java
@@ -5,6 +5,7 @@ public class UserTest {
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		System.out.println("Hello scott");
+		System.out.println("Have a problem");
 	}
 
 }
