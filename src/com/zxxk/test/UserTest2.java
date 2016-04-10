08cb5bbbf361aecbf1dd9b711f3cdc23f5b7f784
xxg <xxg@192.168.1.101>
2016-04-10 13:24:13 +0800

    test add file

---
+++ src/com/zxxk/test/UserTest2.java
@@ -0,0 +1,11 @@
+package com.zxxk.test;
+
+public class UserTest {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		System.out.println("Hello scott");
+		System.out.println("Have a problem");
+	}
+
+}
